f00201491192ef4b0fc6f48de280c6710aeef8c3
Başak Oktay <bskokty@gmail.com>
2026-02-02 00:39:52 +0300

    fix3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // PWA ve metadata için optimizasyonlar
+  swcMinify: true,
+  compress: true,
 };
 
 module.exports = nextConfig;
